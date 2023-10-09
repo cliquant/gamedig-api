e150442dbe581cee0d34b81f8e804a8039ca75e5
Niks <42009476+cliquant@users.noreply.github.com>
2023-10-09 22:54:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,151 @@
-const Gamedig = require('gamedig');
 const express = require('express');
-const http = require('http');
-const app = express();
-const server = http.createServer(app);
-const port = 1000;
+const Gamedig = require('gamedig');
 
-app.use(express.urlencoded({ extended: false }));
+const app = express();
+const port = process.env.PORT || 1000;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.status(200).json({status: 'error', message: 'not available'});
-})
-
-app.get('/info', (req, res) => {
-    const { game, ip, port } = req.query;
-
-    if(!game || !ip || !port) {
-        return res.status(200).json({status: 'error', message: 'missing required fields'});
-    }
-    
-    Gamedig.query({
-        type: game,
-        host: ip,
-        port: port
-    }).then((state) => {
-        res.status(200).json({status: 'success', message: 'online', game: game, ipport: ip + ":" + port, ping: state.ping, playercount: state.players.length, maxplayercount: state.maxplayers,  });
-    }).catch((error) => {
-        console.log(error);
-        return res.status(200).json({status: 'error', message: 'offline', game: game, ipport: ip + ":" + port});
-    });
+  res.status(200).json({ status: 'success', message: 'API is running' });
 });
 
-app.post('/info', (req, res) => {
-    const { game, ip, port } = req.body;
+app.get('/info', async (req, res) => {
+  const { game, ip, port } = req.query;
 
-    if(!game || !ip || !port) {
-        return res.status(200).json({status: 'error', message: 'missing required fields'});
-    }
-    
-    Gamedig.query({
-        type: game,
-        host: ip,
-        port: port
-    }).then((state) => {
-        res.status(200).json({status: 'success', message: 'online', game: game, ipport: ip + ":" + port, ping: state.ping, playercount: state.players.length, maxplayercount: state.maxplayers,  });
-    }).catch((error) => {
-        console.log(error);
-        return res.status(200).json({status: 'error', message: 'offline', game: game, ipport: ip + ":" + port});
+  if (!game || !ip || !port) {
+    return res.status(400).json({ status: 'error', message: 'Missing required fields' });
+  }
+
+  try {
+    const state = await Gamedig.query({
+      type: game,
+      host: ip,
+      port: port,
     });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Server is online',
+      game: game,
+      ipport: `${ip}:${port}`,
+      ping: state.ping,
+      playercount: state.players.length,
+      maxplayercount: state.maxplayers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve server information',
+      game: game,
+      ipport: `${ip}:${port}`,
+    });
+  }
 });
 
-server.listen(port, () => {
-    console.log(`Api listening on port ${port}`)
-})
-  
+app.post('/info', async (req, res) => {
+  const { game, ip, port } = req.body;
+
+  if (!game || !ip || !port) {
+    return res.status(400).json({ status: 'error', message: 'Missing required fields' });
+  }
+
+  try {
+    const state = await Gamedig.query({
+      type: game,
+      host: ip,
+      port: port,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Server is online',
+      game: game,
+      ipport: `${ip}:${port}`,
+      ping: state.ping,
+      playercount: state.players.length,
+      maxplayercount: state.maxplayers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve server information',
+      game: game,
+      ipport: `${ip}:${port}`,
+    });
+  }
+});
+
+app.get('/players', async (req, res) => {
+  const { game, ip, port } = req.query;
+
+  if (!game || !ip || !port) {
+    return res.status(400).json({ status: 'error', message: 'Missing required fields' });
+  }
+
+  try {
+    const state = await Gamedig.query({
+      type: game,
+      host: ip,
+      port: port,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Player list retrieved',
+      game: game,
+      ipport: `${ip}:${port}`,
+      players: state.players,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve player list',
+      game: game,
+      ipport: `${ip}:${port}`,
+    });
+  }
+});
+
+app.get('/server-status', async (req, res) => {
+  const { game, ip, port } = req.query;
+
+  if (!game || !ip || !port) {
+    return res.status(400).json({ status: 'error', message: 'Missing required fields' });
+  }
+
+  try {
+    const state = await Gamedig.query({
+      type: game,
+      host: ip,
+      port: port,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Server status retrieved',
+      game: game,
+      ipport: `${ip}:${port}`,
+      online: true,
+      ping: state.ping,
+      playercount: state.players.length,
+      maxplayercount: state.maxplayers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(200).json({
+      status: 'success',
+      message: 'Server is offline',
+      game: game,
+      ipport: `${ip}:${port}`,
+      online: false,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API listening on port ${port}`);
+});
